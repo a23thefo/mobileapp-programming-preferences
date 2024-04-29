819c2c3629803a56b620652d149cc38dc69eb8d4
Nixeff <theodore.folkesson@gmail.com>
2024-04-29 10:15:21 +0200

    Gjorde ny activity

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
